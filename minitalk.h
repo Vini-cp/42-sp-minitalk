1a9b2131a80a21205882e309199dbf54ca4b6d4f
Vinícius Cordeiro Pereira <viniciuscordeirop@gmail.com>
2022-02-01 22:54:34 +0100

    Norminette

--- minitalk.h
+++ minitalk.h
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minitalk.h                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: coder <coder@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/02/01 22:52:02 by coder             #+#    #+#             */
+/*   Updated: 2022/02/01 22:53:05 by coder            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #ifndef MINITALK_H
 # define MINITALK_H
 
@@ -9,10 +21,10 @@
 # define SLEEP_TIME 100
 # define BYTE_SIZE 8
 
-int	  ft_strlen(const char *s);
-int	  ft_atoi(const char *str);
+int		ft_strlen(const char *s);
+int		ft_atoi(const char *str);
 char	*ft_itoa(int num);
-char  *ft_concatenate(char const *str, char c);
+char	*ft_concatenate(char const *str, char c);
 char	*ft_strdup(const char *s);
 
 #endif
